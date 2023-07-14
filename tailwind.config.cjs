a15afba0ab2f1d077b0e7165d9540a2bfdf22ac0
Ivan Martinez <jesulim@gmail.com>
2023-07-13 22:37:28 -0400

    add component Header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark-gray': '#1F2123',
+				'red': '#FF5145',
+				'green': '#38A852',
+				'blue': '#3D8BFF',
+				'yellow': '#FFBB01',
+				'white': '#FEFEFE',
+			},
+		},
 	},
 	plugins: [],
 }
