4b401f7c054a5ae8cc7a66567321c29940d9a91c
Grover101 <limachigrover09@gmail.com>
2023-07-15 11:33:28 -0400

    formatted code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'dark-gray': '#1F2123',
-				'red': '#FF5145',
-				'green': '#38A852',
-				'blue': '#3D8BFF',
-				'yellow': '#FFBB01',
-				'white': '#FEFEFE',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#1F2123',
+        red: '#FF5145',
+        green: '#38A852',
+        blue: '#3D8BFF',
+        yellow: '#FFBB01',
+        white: '#FEFEFE'
+      }
+    }
+  },
+  plugins: []
 }
