19db840c40e4aece564d5bb2636cb6a057421862
Grover101 <limachigrover09@gmail.com>
2023-07-22 09:15:34 -0400

    add hook timer

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'preact/hooks'
+
+type RemainingDate<T> = {
+  days: T
+  hours: T
+  minutes: T
+  seconds: T
+}
+
+const localeDate = () => {
+  const date = new Date()
+  return new Date(date.toLocaleString('en-US'))
+}
+
+const mapValues = (
+  object: { [key: string]: unknown },
+  iterator: (key: unknown) => void
+) => {
+  return Object.keys(object).reduce((acc, key) => {
+    acc[key] = iterator(object[key])
+    return acc
+  }, {})
+}
+
+const alwaysPositive = (value: number) => Math.max(0, value)
+
+const getRemainingTime = (targetDate: Date) => {
+  const currentDate = localeDate()
+
+  const difference = targetDate.getTime() - currentDate.getTime()
+  const days = alwaysPositive(Math.floor(difference / (1000 * 60 * 60 * 24)))
+  const hours = alwaysPositive(
+    Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  )
+  const minutes = alwaysPositive(
+    Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+  )
+  const seconds = alwaysPositive(Math.floor((difference % (1000 * 60)) / 1000))
+  return { days, hours, minutes, seconds }
+}
+
+const fillZeros = (remainingDate: RemainingDate<number>) => {
+  return mapValues(remainingDate, value =>
+    `${value}`.padStart(2, '0')
+  ) as RemainingDate<string>
+}
+
+export const useTimer = (targetDate: Date) => {
+  const [remainingDate, setRemainingDate] = useState(
+    getRemainingTime(targetDate)
+  )
+
+  const { seconds, minutes, hours, days } = remainingDate
+  const continueTime =
+    days === 0 && hours === 0 && minutes === 0 && seconds === 0
+
+  useEffect(() => {
+    const timer =
+      !continueTime &&
+      setInterval(() => {
+        setRemainingDate(getRemainingTime(targetDate))
+      }, 1000)
+
+    if (continueTime) clearInterval(timer)
+
+    return () => clearInterval(timer)
+  }, [continueTime])
+
+  return { ...fillZeros(remainingDate), continueTime }
+}
