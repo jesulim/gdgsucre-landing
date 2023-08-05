a45095a347953a6d5efacd3ea704f874b503fdc5
pablonoya <pablonoya18@live.com>
2023-08-04 23:21:20 -0400

    upgrade schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -3,34 +3,65 @@ import { useState, useEffect } from 'react'
 
 import { firestore } from '../firebase/client'
 
-const ScheduleCard = ({ type, schedule, title, name, technologyType }) => {
+const ScheduleCard = ({
+  type,
+  schedule,
+  photoUrl,
+  title,
+  name,
+  description,
+  technologyType
+}) => {
   const textColor = type === 'Actividad' ? 'text-green' : 'text-yellow'
 
   return (
-    <div className='mx-auto w-full max-w-screen-lg p-4 border rounded-lg shadow sm:p-8 bg-white bg-opacity-20 border-gray-700'>
+    <div className='mx-auto w-full max-w-screen-lg py-2 px-4 border rounded-lg shadow sm:p-8 bg-white bg-opacity-20 border-gray-700'>
       <div className='flow-root'>
         <ul role='list' className='divide-y divide-gray-700'>
           <li className='text-white'>
-            <div className='flex space-x-1 mb-2'>
+            <div className='flex items-start space-x-4'>
+              <div className='flex flex-col items-center justify-center space-y-3'>
+                <h3 className='text-2xl font-bold'>{schedule}</h3>
+                {photoUrl && (
+                  <img
+                    className='w-10 h-10 rounded-full'
+                    src={photoUrl}
+                    alt={name}
+                  />
+                )}
+              </div>
+              <div className='flex-1 overflow-hidden'>
+                <p
+                  className={
+                    'text-xl md:text-2xl font-bold line-clamp-3 ' + textColor
+                  }
+                >
+                  {title}
+                </p>
+                <p className='text-md md:text-lg font-medium text-white opacity-80'>
+                  {name}
+                </p>
+                <p className='text-lg md:text-xl text-white line-clamp-3'>
+                  {description}
+                </p>
+              </div>
+            </div>
+            <div className='flex space-x-1'>
               {type !== 'Actividad' && (
-                <span className='text-lg ml-auto bg-blue rounded-full px-2'>
+                <span
+                  className={
+                    'mt-2 text-sm md:text-lg ml-auto rounded-full px-2 ' +
+                    (type === 'Conferencia' ? 'bg-blue' : 'bg-red')
+                  }
+                >
                   {type}
                 </span>
               )}
               {technologyType && (
-                <span className='text-lg  bg-green rounded-full px-2'>
+                <span className='mt-2 text-sm md:text-lg  bg-green rounded-full px-2'>
                   {technologyType}
                 </span>
               )}
-            </div>
-            <div className='flex items-center space-x-4'>
-              <span className='font-bold text-2xl sm:text-5xl'>{schedule}</span>
-              <div className='flex-1 overflow-hidden'>
-                <p className={'text-xl md:text-2xl font-bold ' + textColor}>
-                  {title}
-                </p>
-                <p className='text-md md:text-lg truncate text-white'>{name}</p>
-              </div>
             </div>
           </li>
         </ul>
