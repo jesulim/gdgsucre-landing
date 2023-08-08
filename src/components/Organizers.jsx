3751e74949d587ad673364631222c4f978f2d806
pablonoya <pablonoya18@live.com>
2023-08-08 11:28:13 -0400

    order and colorize organizers by type

--- src/components/Organizers.jsx
+++ src/components/Organizers.jsx
@@ -1,13 +1,23 @@
-import { collection, onSnapshot, query } from 'firebase/firestore'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 import { useState, useEffect } from 'react'
 
 import { firestore } from '../firebase/client'
+
+const accentColorByType = {
+  4: 'red',
+  3: 'yellow',
+  2: 'green',
+  1: 'blue'
+}
 
 export const Organizers = () => {
   const [organizersData, setOrganizersData] = useState([])
 
   useEffect(() => {
-    const sheduleCollection = query(collection(firestore, 'organizers'))
+    const sheduleCollection = query(
+      collection(firestore, 'organizers'),
+      orderBy('type', 'desc')
+    )
 
     onSnapshot(sheduleCollection, snapshot => {
       const organizerDocs = snapshot.docs.map(doc => ({
@@ -19,18 +29,28 @@ export const Organizers = () => {
   }, [])
 
   return (
-    <div className='mx-auto w-full max-w-screen-md px-2'>
-      <div className='grid grid-cols-3 gap-6 sm:gap-8 sm:grid-cols-4'>
-        {organizersData.map(({ id, name, photoUrl }) => (
-          <div className='text-center w-32 h-32' key={id}>
-            <img
-              className='w-24 h-24 rounded-full mx-auto border-2 border-black'
-              src={photoUrl}
-              alt={name}
-            />
-            <span className='text-white'>{name}</span>
-          </div>
-        ))}
+    <div className='mx-auto w-full max-w-screen-md '>
+      <div className='grid grid-cols-3 gap-x-2 gap-y-8 sm:gap-8 sm:grid-cols-4'>
+        {organizersData.map(({ id, type, name, photoUrl, link }) => {
+          const accentColor = accentColorByType[type]
+
+          return (
+            <a
+              className={`text-white text-center w-32 h-32 hover:text-${accentColor}`}
+              key={id}
+              href={link}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                className='w-24 h-24 rounded-full mx-auto border-2 border-black'
+                src={photoUrl}
+                alt={name}
+              />
+              <span>{name}</span>
+            </a>
+          )
+        })}
       </div>
     </div>
   )
