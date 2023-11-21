32f61de5b7f66c8c6274a2ef56b10c62ed9fb347
Ivan Martinez <jesulim@gmail.com>
2023-11-21 01:14:27 -0400

    change Timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -15,9 +15,9 @@ export const Timer = ({ targetDate }: TimerProps) => {
 
   return (
     <>
-      {continueTime && (
-        <div className='mb-2 font-bold'>Empezo Google I/O Sucre 2023</div>
-      )}
+      {/* {continueTime && (
+        <div className='mb-2 font-bold'>Empezó Devfest Sucre 2023</div>
+      )} */}
 
       <section className='flex justify-center'>
         {time.map(({ label, value }, index) => {
@@ -26,7 +26,7 @@ export const Timer = ({ targetDate }: TimerProps) => {
           return (
             <div className='flex-col w-20 sm:w-24 lg:w-36' key={label}>
               <div
-                className={`text-yellow font-bold text-5xl lg:text-7xl relative ${
+                className={`text-white font-bold text-5xl lg:text-7xl relative ${
                   !isLast &&
                   'after:ml-2 after:font-bold after:text-yellow after:content-[":"] after:absolute lg:after:ml-5'
                 }`}
@@ -34,7 +34,7 @@ export const Timer = ({ targetDate }: TimerProps) => {
                 {value}
               </div>
               {label && (
-                <span className='text-white text-xs lg:text-base'>{label}</span>
+                <span className='text-yellow text-xs lg:text-base'>{label}</span>
               )}
             </div>
           )
