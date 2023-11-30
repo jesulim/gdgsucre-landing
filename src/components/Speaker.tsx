8aa7f20c3b99a96bb7c386ae24c7e3179a74f789
pablonoya <pablonoya18@live.com>
2023-11-29 23:04:16 -0400

    add speakers

--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -1,32 +1,37 @@
 import Atropos from 'atropos/react'
 import 'atropos/css'
-
+// import speakerBg from '../assets/speaker-bg.png'
 export interface Props {
   id: number
   img: string
-  speaker: string
+  name: string
 }
 
-export const Speaker = ({ id, img, speaker }: Props) => {
+export const Speaker = ({ id, img, name }: Props) => {
   return (
     <Atropos className='atropos-banner' rotateTouch='scroll-y'>
       <div className='relative' key={id}>
         <img
+          src='/speaker-bg.png'
+          className='w-full h-full'
+          data-atropos-offset='0'
+        />
+        <img
           src={img}
-          alt={speaker}
-          className='w-auto h-60 lg:h-80 xl:h-96 object-cover object-center'
+          alt={name}
+          className='absolute inset-0 mx-auto h-full  object-cover'
           data-atropos-offset='-4.5'
         />
-        <div className='absolute bottom-5 left-1/2 transform -translate-x-1/2'>
+        <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2'>
           <div
             data-atropos-offset='-2'
-            className='p-0 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow relative inline-block w-max'
+            className='px-2 bg-red relative inline-block w-max'
           >
             <span
               data-atropos-offset='4.5'
-              className='relative inline-block text-dark-gray text-lg md:text-2xl'
+              className='relative inline-block text-black text-lg md:text-2xl'
             >
-              {speaker}
+              {name}
             </span>
           </div>
         </div>
