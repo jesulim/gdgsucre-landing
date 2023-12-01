506cf348803a2e3bbe030bd79b62ed6f0ec26b97
pablonoya <pablonoya18@live.com>
2023-12-01 00:42:50 -0400

    finish schedule

--- src/components/ScheduleCard.jsx
+++
@@ -1,9 +0,0 @@
-import { ActivityCard } from './ScheduleCards/ActivityCard'
-import { SpeakerCard } from './ScheduleCards/SpeakerCard'
-
-export const ScheduleCard = props => {
-  if (props.type === 'Actividad') {
-    return <ActivityCard {...props} />
-  }
-  return <SpeakerCard {...props} />
-}
