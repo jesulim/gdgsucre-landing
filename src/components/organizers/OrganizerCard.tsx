d604c033849aba597395c92c03b2051f7ad17e64
pablonoya <pablonoya18@live.com>
2024-07-12 02:04:10 -0400

    Update organizers

--- src/components/organizers/OrganizerCard.tsx
+++ src/components/organizers/OrganizerCard.tsx
@@ -1,31 +1,39 @@
 export interface Props {
-  id: string;
-  name: string;
-  photoUrl: string;
-  socialNetworks: { type: string; url: string }[];
+  id: string
+  name: string
+  photoUrl: string
+  socialNetworks: { type: string; url: string }[]
 }
 
 export const OrganizerCard = ({ id, name, photoUrl }: Props) => {
   return (
-    <div className="w-full max-w-[150px] sm:max-w-xs">
-      <div className='relative aspect-[4/5]' key={id}>
+    <div className='w-full max-w-[150px] sm:max-w-xs'>
+      <div className='relative aspect-[4/6]' key={id}>
         <div className='absolute top-1 sm:top-2 -right-2 sm:-right-4 w-full h-full bg-gradient-to-t from-green via-blue via-50% to-blue rounded-2xl sm:rounded-3xl shadow-sm transform rotate-2 sm:rotate-4 border-2 border-black dark:border-white sm:border-2'></div>
         <div className='absolute top-0.5 sm:top-1 -right-1 sm:-right-2 w-full h-full bg-gradient-to-t from-green via-green via-75% to-blue rounded-2xl sm:rounded-3xl shadow-sm transform rotate-1 sm:rotate-2 border-2 border-black dark:border-white sm:border-2'></div>
 
         <div className='absolute inset-0 bg-white dark:bg-dark-background rounded-2xl sm:rounded-3xl shadow-md overflow-hidden border-2 border-black dark:border-white sm:border-2'>
           <div className='p-2 sm:p-4 lg:p-6 flex flex-col items-center justify-center h-full'>
-            <div className='w-16 h-16 sm:w-24 sm:h-24 lg:w-32 lg:h-32 rounded-full overflow-hidden mb-2 sm:mb-4 border border-gray-300 dark:border-white sm:border-2'>
+            <div className='p-4 md:p-8'>
               <img
+                className='aspect-square object-cover rounded-full border-2 border-black dark:border-white'
+                width='300'
+                height='300'
                 src={photoUrl}
                 alt={name}
-                className='w-full h-full object-cover'
+                loading='lazy'
+                decoding='async'
               />
             </div>
-            <h2 className='text-sm sm:text-lg lg:text-xl font-bold text-center mb-1 dark:text-dark-text'>{name}</h2>
-            <p className="text-sm sm:text-lg lg:text-xl font-light text-center mb-1 dark:text-dark-text">Organizer</p>
+            <h2 className='text-sm sm:text-lg lg:text-xl font-bold text-center mb-1 dark:text-dark-text'>
+              {name}
+            </h2>
+            <p className='text-sm sm:text-lg lg:text-xl font-light text-center mb-1 dark:text-dark-text'>
+              Organizer
+            </p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
