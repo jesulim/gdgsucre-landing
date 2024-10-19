e5f11461c8933628b940eb22d83c06c2be5a166d
pablonoya <pablonoya18@live.com>
2024-10-18 23:38:03 -0400

    Add dietary restricction to form, delete session cookie on invalid verification

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -11,13 +11,17 @@ export const server = {
       uid: z.string(),
       firstname: z.string(),
       lastname: z.string(),
-      email: z.string(),
-      phone: z.string()
+      email: z.string().email(),
+      phone: z.string(),
+
+      dietaryRestriction: z
+        .string()
+        .optional()
+        .transform(val => val || '')
     }),
     handler: async input => {
       const { uid, ...data } = input
 
-      console.log(uid, data)
       const db = getFirestore(app).collection('registro-devfest2024')
       await db.doc(uid).set(data)
     }
