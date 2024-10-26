84a279279a2cd1f13fbf968db45c3ae82690fc7e
pablonoya <pablonoya18@live.com>
2024-10-25 23:08:36 -0400

    Update registration

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,26 +4,33 @@ import { z } from 'astro:schema'
 import { getFirestore } from 'firebase-admin/firestore'
 import { app } from 'src/firebase/server'
 
+const schema = z.object({
+  uid: z.string(),
+  firstname: z.string(),
+  lastname: z.string(),
+  email: z.string().email(),
+  phone: z.string(),
+  ci: z.string(),
+  cu: z.string().nullish().default(''),
+  reasonToParticipate: z.string().nullish().default(''),
+  dietaryRestriction: z.string().nullish().default('')
+})
+
+const sendToFirestore = async input => {
+  const { uid, ...data } = input
+
+  data.validated = false
+  data.checkin = false
+  data.lunch = false
+
+  const db = getFirestore(app).collection('registro-devfest2024')
+  await db.doc(uid).set(data)
+}
+
 export const server = {
   register: defineAction({
     accept: 'form',
-    input: z.object({
-      uid: z.string(),
-      firstname: z.string(),
-      lastname: z.string(),
-      email: z.string().email(),
-      phone: z.string(),
-
-      dietaryRestriction: z
-        .string()
-        .optional()
-        .transform(val => val || '')
-    }),
-    handler: async input => {
-      const { uid, ...data } = input
-
-      const db = getFirestore(app).collection('registro-devfest2024')
-      await db.doc(uid).set(data)
-    }
+    input: schema,
+    handler: sendToFirestore
   })
 }
