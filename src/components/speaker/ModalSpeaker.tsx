0c7d614bcf523b289c770cb7a1ed050c7f87d129
pablonoya <pablonoya18@live.com>
2024-11-07 16:41:59 -0400

    Update speakers modal and card

--- src/components/speaker/ModalSpeaker.tsx
+++ src/components/speaker/ModalSpeaker.tsx
@@ -1,6 +1,8 @@
+import SocialIcons from './SocialIcons.tsx'
+
 interface ModalPropsType {
   isOpen: boolean
-  onClose: () => void;
+  onClose: () => void
   data: {
     name: string
     descriptions?: Array<string>
@@ -9,62 +11,81 @@ interface ModalPropsType {
 }
 
 export interface SocialNetworksType {
-  href: string
-  svg: string
+  name: string
+  url: string
 }
+
 export const ModalSpeaker = ({ isOpen, onClose, data }: ModalPropsType) => {
-  if (!isOpen) return null;
+  if (!isOpen) return null
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50">
-      <div className="relative w-full max-w-2xl rounded-lg shadow dark:bg-gray-700 p-4 bg-white dark:bg-black">
-        {/* Modal header */}
-        <div className="flex items-center justify-between p-4 border-b rounded-t dark:border-gray-600">
-          <h3 className="uppercase text-xl font-semibold text-gray-900 dark:text-white">
-            {data.name}
-          </h3>
-          <button
-            type="button"
-            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            onClick={onClose}
+    <div
+      className='pointer-events-auto fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black/60'
+      onClick={onClose}
+    >
+      <div
+        className='max-w-xs bg-black p-0.5 dark:bg-white sm:max-w-xl md:max-w-full'
+        style={{
+          clipPath:
+            'polygon(calc(100% - 1rem) 0%, 100% 1rem, 100% 100%, 1rem 100%, 0 calc(100% - 1rem), 0 0'
+        }}
+      >
+        <div
+          className='bg-blue pb-4 pr-4'
+          style={{
+            clipPath:
+              'polygon(calc(100% - 1rem) 0%, 100% 1rem, 100% 100%, 1rem 100%, 0 calc(100% - 1rem), 0 0'
+          }}
+        >
+          <div
+            className='flex max-h-screen w-full max-w-2xl flex-col border-b-2 border-r-2 bg-white dark:bg-black'
+            onClick={e => e.stopPropagation()}
           >
-            <svg className="h-8 w-8 text-red-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
-            <span className="sr-only">Close modal</span>
-          </button>
-        </div>
+            {/* Modal header */}
+            <div className='flex items-center justify-between border-b-2 border-black bg-blue px-1 py-2 text-white dark:border-white md:p-2'>
+              <h3 className='ml-2 text-xl font-bold'>{data.name}</h3>
+              <button
+                type='button'
+                className='flex h-8 w-8 items-center justify-center border-2 bg-gray text-black'
+                onClick={onClose}
+              >
+                <svg
+                  className='text-red-500 h-8 w-8'
+                  width='24'
+                  height='24'
+                  viewBox='0 0 24 24'
+                  strokeWidth='2'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                >
+                  <path stroke='none' d='M0 0h24v24H0z' /> <line x1='18' y1='6' x2='6' y2='18' />{' '}
+                  <line x1='6' y1='6' x2='18' y2='18' />
+                </svg>
+                <span className='sr-only'>Close modal</span>
+              </button>
+            </div>
 
-        {/* <!-- Modal body --> */}
-        <div className="p-4 md:p-5 space-y-4">
-          {data.descriptions && (
-            <>
-              {data.descriptions.map((description: string, index: number) => (
-
-                <p className='text-black text-sm sm:text-md md:text-xl dark:text-white'>
-                  {description}
-                </p>
-
-              ))
-              }
-            </>
-          )}
-
-        </div>
-        {/* <!-- Modal footer --> */}
-        <div className="flex items-center p-4 md:p-5 border-gray-200 rounded-b dark:border-gray-600">
-          {data.socialNetworks && (
-            <>
-              {data.socialNetworks.map((icon, index) => (
-                <a
-                  key={index}
-                  href={icon.href}
-                  className="mr-2"
-                  dangerouslySetInnerHTML={{ __html: icon.svg }}
-                ></a>
-              ))}
-            </>
-          )}
+            {/* <!-- Modal body --> */}
+            <div className='flex max-h-[70vh] flex-col space-y-4 overflow-scroll p-4'>
+              {data.descriptions && (
+                <>
+                  {data.descriptions.map((description: string, index: number) => (
+                    <p
+                      key={index}
+                      className='sm:text-md text-sm text-black dark:text-white md:text-xl'
+                    >
+                      {description}
+                    </p>
+                  ))}
+                </>
+              )}
+              <SocialIcons socialNetworks={data.socialNetworks} />
+            </div>
+          </div>
         </div>
       </div>
-    </div >
-  );
-};
+    </div>
+  )
+}
--- src/components/speaker/Speakers.tsx
+++ src/components/speaker/Speakers.tsx
@@ -17,11 +17,17 @@ export interface Props {
 export const Speaker = ({ id, img, name, location, flag, descriptions, socialNetworks }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
-  const openModal = () => setIsModalOpen(true)
-  const closeModal = () => setIsModalOpen(false)
+  const openModal = () => {
+    setIsModalOpen(true)
+    document.body.style.overflow = 'hidden'
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+    document.body.style.overflow = 'unset'
+  }
 
   return (
-    <>
+    <div key={id}>
       {/* Modal */}
       <ModalSpeaker
         isOpen={isModalOpen}
@@ -30,29 +36,24 @@ export const Speaker = ({ id, img, name, location, flag, descriptions, socialNet
       />
 
       {/* Cards */}
-      <div
-        key={id}
-        className='transition-duration-300 h-full pb-4 pl-4 mix-blend-luminosity transition-all hover:!scale-100 md:group-hover:scale-[0.90] md:group-hover:blur-[1px] hover:!blur-none'
-      >
+      <div className='transition-duration-300 pointer-events-auto h-full pb-4 pl-4 mix-blend-luminosity transition-all hover:!scale-100 hover:!blur-none md:group-hover:scale-[0.90] md:group-hover:blur-[1px]'>
         <div className='relative flex h-full w-full flex-col items-center space-y-2 rounded-2xl border-2 bg-white px-2 pb-4 text-center dark:bg-black md:rounded-3xl md:pb-8 md:pt-4'>
           <div
             className={`absolute -bottom-2 -left-2 -z-10 h-full w-full rounded-2xl border-2 sm:-bottom-3 sm:-left-3 md:rounded-3xl ${bgColors[id % 4]}`}
-            data-atropos-offset='-1'
           ></div>
 
           <img
             src={img}
             alt={name}
             className='w-11/12 rounded-full md:w-48'
-            data-atropos-offset='1'
             decoding='async'
             loading='lazy'
             width='500'
             height='500'
           />
 
-          <span className='font-semibold leading-tight sm:text-2xl'>{name}</span>
-          <div className='flex flex-row items-center justify-center' data-atropos-offset='0'>
+          <span className='py-2 font-semibold leading-tight sm:text-2xl'>{name}</span>
+          <div className='flex flex-row items-center pb-2'>
             <img
               width='30'
               height='15'
@@ -64,11 +65,11 @@ export const Speaker = ({ id, img, name, location, flag, descriptions, socialNet
             />
             <p className='text-xs sm:text-xl'>{location}</p>
           </div>
-          <button className='text-blue' onClick={openModal}>
+          <button className='hover: mt-auto text-blue' onClick={openModal}>
             Ver biografía
           </button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
