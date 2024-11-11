6e1b26c0fd4b5ea8ef0ef1404dff664553849c51
Ivan Martinez <jesulim@gmail.com>
2024-11-10 20:55:22 -0400

    feat(EventBadge): add Arroba icon and improve layout structure

---
+++ public/arroba.tsx
@@ -0,0 +1,32 @@
+const Arroba = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={33}
+    height={37}
+    fill='none'
+    {...props}
+  >
+    <path
+      stroke='#1E1E1E'
+      strokeLinejoin='round'
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d='M24.506 22.582c-3.046 5.672-9.247 8.236-13.852 5.731-4.603-2.505-5.865-9.133-2.817-14.805M7.837 13.508c3.046-5.672 9.247-8.237 13.852-5.732 4.602 2.505 5.865 9.134 2.817 14.806'
+    />
+    <path
+      stroke='#1E1E1E'
+      strokeLinejoin='round'
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d='M28.87 24.819c-.787 1.465-2.391 2.13-3.58 1.482-1.188-.647-1.515-2.362-.727-3.826M18.935 34.042c-3.732 1.468-7.723 1.447-11.137-.41-6.985-3.803-8.902-13.867-4.275-22.474'
+    />
+    <path
+      stroke='#1E1E1E'
+      strokeLinejoin='round'
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d='M3.52 11.158c4.623-8.608 14.036-12.503 21.024-8.7 6.986 3.803 8.902 13.867 4.276 22.474'
+    />
+  </svg>
+)
+export default Arroba
