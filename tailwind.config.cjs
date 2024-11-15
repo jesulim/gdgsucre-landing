09c6d8c552987e0477871f042190de6e270bcc62
fernando <vluisfernando62@gmail.com>
2024-11-15 12:54:40 -0400

    fix: remove background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,6 @@ module.exports = {
     extend: {
       backgroundImage: {
         'sponsors-pattern': 'url("./backround-patron.svg")',
-        'circuit-light':
-          'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100"><rect width="100" height="100" fill="%23ffffff"/><circle cx="10" cy="10" r="2" fill="%234283f4"/><circle cx="50" cy="50" r="2" fill="%23db4437"/><circle cx="90" cy="90" r="2" fill="%23f4b400"/><line x1="10" y1="10" x2="50" y2="50" stroke="%234283f4" stroke-width="1"/><line x1="50" y1="50" x2="90" y2="90" stroke="%23db4437" stroke-width="1"/></svg>\')',
-        'circuit-dark':
-          'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100"><rect width="100" height="100" fill="%23202124"/><circle cx="10" cy="10" r="2" fill="%234283f4"/><circle cx="50" cy="50" r="2" fill="%23db4437"/><circle cx="90" cy="90" r="2" fill="%23f4b400"/><line x1="10" y1="10" x2="50" y2="50" stroke="%234283f4" stroke-width="1"/><line x1="50" y1="50" x2="90" y2="90" stroke="%23db4437" stroke-width="1"/></svg>\')',
       },
       colors: {
         'dark-gray': '#1F2123',
