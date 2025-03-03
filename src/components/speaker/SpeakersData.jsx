1822ddb09f0acb8766189bafa5b59b75660f743e
CrespoReJha <jhamilcresporejas2004@gmail.com>
2025-03-02 22:40:18 -0400

    plantilla speakers terminada

---
+++ src/components/speaker/SpeakersData.jsx
@@ -0,0 +1,54 @@
+export const speakersData = [
+  {
+    image: 'speakers/FotoPrueba4SinFondo.svg',
+    area: 'WEB',
+    name: 'Pedro Lopez',
+    location: 'Sucre, Bolivia',
+    flag: 'bolivia.svg',
+    social: {
+      github: 'https://github.com/pedrolopez',
+      twitter: 'https://twitter.com/pedrolopez',
+      instagram: 'https://instagram.com/pedrolopez',
+      linkedin: 'https://linkedin.com/in/pedrolopez'
+    }
+  },
+  {
+    image: 'speakers/FotoPrueba4SinFondo.svg',
+    area: 'UX/UI',
+    name: 'Lesly Zerna',
+    location: 'Sucre, Bolivia',
+    flag: 'bolivia.svg',
+    social: {
+      github: 'https://github.com/leslyzerna',
+      twitter: 'https://twitter.com/leslyzerna',
+      instagram: 'https://instagram.com/leslyzerna',
+      linkedin: 'https://linkedin.com/in/lelsyzerna'
+    }
+  },
+  {
+    image: 'speakers/FotoPrueba4SinFondo.svg',
+    area: 'UX/UI',
+    name: 'Lesly Zerna',
+    location: 'Sucre, Bolivia',
+    flag: 'bolivia.svg',
+    social: {
+      github: 'https://github.com/leslyzerna',
+      twitter: 'https://twitter.com/leslyzerna',
+      instagram: 'https://instagram.com/leslyzerna',
+      linkedin: 'https://linkedin.com/in/lelsyzerna'
+    }
+  },
+  {
+    image: 'speakers/FotoPrueba4SinFondo.svg',
+    area: 'UX/UI',
+    name: 'Lesly Zerna',
+    location: 'Sucre, Bolivia',
+    flag: 'bolivia.svg',
+    social: {
+      github: 'https://github.com/leslyzerna',
+      twitter: 'https://twitter.com/leslyzerna',
+      instagram: 'https://instagram.com/leslyzerna',
+      linkedin: 'https://linkedin.com/in/lelsyzerna'
+    }
+  }
+]
