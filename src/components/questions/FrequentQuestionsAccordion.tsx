387b81c918b191390a96006c4cca36c8f31fac17
arielpa <arielpachecoalejandro@gmail.com>
2025-03-11 10:58:27 -0400

    fix: update padding and border color

--- src/components/questions/FrequentQuestionsAccordion.tsx
+++ src/components/questions/FrequentQuestionsAccordion.tsx
@@ -7,17 +7,17 @@ export interface Props {
 
 const FrequentQuestionsAccordion = ({id, question, answer}: Props) => {
     return (
-        <details className="group border border-green rounded-lg shadow-md" key={id}>
-          <summary className="cursor-pointer flex items-center gap-3 p-4 rounded-lg text-lg font-medium transition-all duration-300">
+        <details className="group border border-green-four rounded-lg shadow-md" key={id}>
+          <summary className="cursor-pointer flex items-center gap-3 px-2 pt-2 pb-2 rounded-lg text-lg font-medium transition-all duration-300">
           
           {/* Icono */}
-          <svg className="w-5 h-5 transition-transform group-open:rotate-90 text-green " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg className="w-5 h-5 transition-transform group-open:rotate-90 text-green-four " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
           </svg>
           
           <span>{question}</span>
         </summary>
-        <div className="p-4 text-gray-700">{answer}</div>
+        <div className="px-4 pb-4 text-gray-700">{answer}</div> 
       </details>
 
     )
