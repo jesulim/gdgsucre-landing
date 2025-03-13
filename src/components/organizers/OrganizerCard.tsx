8267f2e3d071e9d1e88cbd4581908d182724f06e
fernando <vluisfernando62@gmail.com>
2025-03-13 16:54:15 -0400

    feat: modificar el componente OrganizerCard y habilitar su visualización

--- src/components/organizers/OrganizerCard.tsx
+++ src/components/organizers/OrganizerCard.tsx
@@ -1,5 +1,11 @@
-import useSvgIcons from './useSvgIcons'
 import './organizerCard.css'
+import {
+  FacebookIcon,
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  WebIcon
+} from '@components/speaker/SocialIcons'
 
 export interface Props {
   id: string
@@ -9,48 +15,55 @@ export interface Props {
   uuid: number
 }
 
-const bgColors = ['bg-yellow', 'bg-green', 'bg-red', 'bg-blue']
+const bgColors = ['bg-[#CAE6FF]', 'bg-[#D1ECE3]']
+const bgColorsDark = ['dark:bg-[#20344B]', 'dark:bg-[#1A3E38]']
 
 const OrganizerCard = ({ photoUrl, name, socialNetworks, uuid }: Props) => {
-  const { gitHubSVG, linkedinSVG, instagramSVG, twitterSVG, facebookSVG, devSVG } = useSvgIcons()
-
   const isSocialNetworksEmpty: boolean = socialNetworks.length === 0
 
   return (
-    <div className='cut-corners-r mt-4 h-full bg-black p-0.5 transition-all dark:bg-white'>
-      <div className={`cut-corners-r h-full pb-2 pr-2 ${bgColors[uuid % 4]}`}>
-        <div className='card h-full border-b-2 border-r-2 bg-white text-center dark:bg-black'>
-          <div className='card-details'>
-            <img
-              className='text-title mx-auto h-48 w-48 rounded-full border-2 object-cover'
-              src={photoUrl}
-              alt='profile'
-              loading='lazy'
-            />
-            <div className='text-body'>
-              <div className='flex items-center justify-around'>
-                {isSocialNetworksEmpty && (
-                  <span className='mt-4 inline-flex items-center rounded-full border bg-white px-3 py-1 shadow-md'>
-                    <span>{devSVG()}</span>
+    <div
+      className={`cut-corners-r mt-4 h-full rounded-3xl border-[3.5px] border-[#20344B] ${bgColors[uuid % 2]} p-0.5 transition-all dark:border-[#CAE6FF] ${bgColorsDark[uuid % 2]}`}
+    >
+      <div className='card-details h-full text-center'>
+        <img
+          className='text-title mx-auto h-48 w-48 rounded-full border-2 object-cover'
+          src={photoUrl}
+          alt='profile'
+          loading='lazy'
+        />
+        <div className='text-body'>
+          <div className='flex items-center justify-center'>
+            {isSocialNetworksEmpty && (
+              <span className='mt-4 inline-flex items-center px-3 py-1'>
+                <span>{<WebIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />}</span>
+              </span>
+            )}
+            {socialNetworks.map((socialNetwork, i) => (
+              <a href={socialNetwork.url} key={i} target='_blank'>
+                <span className='mt-4 inline-flex items-center px-3 py-1'>
+                  <span>
+                    {socialNetwork.type === 'linkedin' && (
+                      <LinkedInIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
+                    )}
+                    {socialNetwork.type === 'github' && (
+                      <GitHubIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
+                    )}
+                    {socialNetwork.type === 'instagram' && (
+                      <InstagramIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
+                    )}
+                    {socialNetwork.type === 'facebook' && (
+                      <FacebookIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
+                    )}
+                    {socialNetwork.type === 'twitter' && (
+                      <WebIcon className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
+                    )}
                   </span>
-                )}
-                {socialNetworks.map((socialNetwork, i) => (
-                  <a href={socialNetwork.url} key={i} target='_blank'>
-                    <span className='mt-4 inline-flex items-center rounded-full border bg-white px-3 py-1 shadow-md'>
-                      <span>
-                        {socialNetwork.type === 'linkedin' && linkedinSVG()}
-                        {socialNetwork.type === 'github' && gitHubSVG()}
-                        {socialNetwork.type === 'instagram' && instagramSVG()}
-                        {socialNetwork.type === 'facebook' && facebookSVG()}
-                        {socialNetwork.type === 'twitter' && twitterSVG()}
-                      </span>
-                    </span>
-                  </a>
-                ))}
-              </div>
-              <h2 className='mt-2 text-2xl font-semibold text-black dark:text-white'>{name}</h2>
-            </div>
+                </span>
+              </a>
+            ))}
           </div>
+          <h2 className='mt-2 text-2xl font-semibold text-[#20344B] dark:text-[#CAE6FF]'>{name}</h2>
         </div>
       </div>
     </div>
